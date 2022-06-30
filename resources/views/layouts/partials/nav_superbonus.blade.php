bdedfb2e1ad4bb002a03c15fdd2de614e223a665
fabioserembe <fabio.serembe@gmail.com>
2022-06-30 15:41:52 +0200

    - Rimossa cartella layouts/partials; - Rimossi componenti non più utilizzati;

--- resources/views/layouts/partials/nav_superbonus.blade.php
+++
@@ -1,8 +0,0 @@
-<div class="nav_bonus d-flex align-items-center ov-x ov-y-none">
-    <a href="{{ route('superbonus.show', [$practice->id, $building->id]) }}" class="{{Route::currentRouteName() == 'superbonus.show' ? 'frame' : ''}} text-nowrap px-3">Dati di Progetto</a>
-    <a href="{{ route('driving_intervention', [$practice, 'PV']) }}" class="{{Route::currentRouteName() == 'driving_intervention' ? 'frame' : ''}} text-nowrap px-3">Interventi trainanti</a>
-    <a href="{{ route('towed_intervention', [$practice, 'common', 'PV']) }}" class="{{Route::currentRouteName() == 'towed_intervention' ? 'frame' : ''}} text-nowrap px-3">Interventi trainati</a>
-    <a href="{{ route('final_state', [$practice]) }}" class="{{Route::currentRouteName() == 'final_state' ? 'frame' : ''}} text-nowrap px-3">Dati stato finale</a>
-    <a href="{{ route('fees_declaration', [$practice]) }}" class="{{Route::currentRouteName() == 'fees_declaration' ? 'frame' : ''}} text-nowrap px-3">Tot. Spese e Dichiarazioni</a>
-    <a href="{{ route('var_computation', [$practice]) }}" class="{{Route::currentRouteName() == 'var_computation' ? 'frame' : ''}} text-nowrap px-3">Varianti</a>
-</div>
