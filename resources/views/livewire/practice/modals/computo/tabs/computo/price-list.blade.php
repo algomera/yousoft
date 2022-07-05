35c9bac4cf7a27c20b05f04158c7947356c90fab
fabioserembe <fabio.serembe@gmail.com>
2022-07-05 12:03:53 +0200

    - Aggiunta icona "cerca" a price-list.blade.php (computo);

--- resources/views/livewire/practice/modals/computo/tabs/computo/price-list.blade.php
+++ resources/views/livewire/practice/modals/computo/tabs/computo/price-list.blade.php
@@ -1,11 +1,18 @@
-<div class="grid grid-cols-10 overflow-hidden">
-	<div class="col-span-10 lg:col-span-2 pr-4">
-		<x-select wire:model="selectedPriceList" name="price_list" id="price_list">
-			@foreach($price_lists as $k => $price_list)
-				<option wire:ignore wire:key="{{$price_list->id}}"
-				        value="{{$price_list->id}}">{{$price_list->name}}</option>
-			@endforeach
-		</x-select>
+<div class="grid grid-cols-10 overflow-hidden mt-3">
+	<div class="col-span-10 lg:col-span-2 pr-4 pt-1">
+		<div class="flex items-center space-x-2 w-full">
+			<div class="flex-1">
+				<x-select wire:model="selectedPriceList" name="price_list" id="price_list" class="!mt-0">
+					@foreach($price_lists as $k => $price_list)
+						<option wire:ignore wire:key="{{$price_list->id}}"
+						        value="{{$price_list->id}}">{{$price_list->name}}</option>
+					@endforeach
+				</x-select>
+			</div>
+			<x-button class="h-[38px]">
+				<x-icon name="search" class="w-4 h-4"></x-icon>
+			</x-button>
+		</div>
 		<nav class="space-y-0 space-x-2 lg:space-x-0 lg:space-y-1.5 flex flex-row h-[450px] overflow-y-auto mt-3 lg:flex-col"
 		     aria-label="Sidebar">
 			<x-price-list-folder-loop :items="$tree" :selected="$selected"></x-price-list-folder-loop>
