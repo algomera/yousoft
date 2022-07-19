5f4165931e6efe1681389dfa825c5b7eec157d04
fabioserembe <fabio.serembe@gmail.com>
2022-07-19 17:26:09 +0200

    - Fix grafici chat;

--- resources/views/vendor/Chatify/layouts/info.blade.php
+++ resources/views/vendor/Chatify/layouts/info.blade.php
@@ -2,7 +2,10 @@
 <div class="avatar av-l chatify-d-flex"></div>
 <p class="info-name">{{ config('chatify.name') }}</p>
 <div class="messenger-infoView-btns">
-    <a href="#" class="danger delete-conversation"><i class="fas fa-trash-alt"></i> Cancella conversazione</a>
+    <a href="#" class="danger delete-conversation !inline-flex items-center justify-center space-x-1">
+        <x-icon name="trash" class="h-4 w-4"></x-icon>
+        <span>Cancella conversazione</span>
+    </a>
 </div>
 {{-- shared photos --}}
 <div class="messenger-infoView-shared">
--- resources/views/vendor/Chatify/layouts/sendForm.blade.php
+++ resources/views/vendor/Chatify/layouts/sendForm.blade.php
@@ -1,8 +1,13 @@
-<div class="messenger-sendCard">
+<div class="messenger-sendCard flex items-center">
     <form id="message-form" method="POST" action="{{ route('send.message') }}" enctype="multipart/form-data">
         @csrf
-        <label><span class="fas fa-paperclip"></span><input disabled='disabled' type="file" class="upload-attachment" name="file" accept=".{{implode(', .',config('chatify.attachments.allowed_images'))}}, .{{implode(', .',config('chatify.attachments.allowed_files'))}}" /></label>
-        <textarea readonly='readonly' name="message" class="m-send app-scroll" placeholder="Type a message.."></textarea>
-        <button disabled='disabled'><span class="fas fa-paper-plane"></span></button>
+        <label class="flex items-center justify-center">
+            <x-icon name="paper-clip" class="h-6 w-6"></x-icon>
+            <input disabled='disabled' type="file" class="upload-attachment" name="file" accept=".{{implode(', .',config('chatify.attachments.allowed_images'))}}, .{{implode(', .',config('chatify.attachments.allowed_files'))}}" />
+        </label>
+        <textarea readonly='readonly' name="message" class="m-send app-scroll" placeholder="Scrivi un messaggio.."></textarea>
+        <button disabled='disabled'>
+            <x-icon name="paper-plane" class="h-6 w-6"></x-icon>
+        </button>
     </form>
 </div>
